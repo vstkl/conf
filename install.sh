bfa0bf1a7214c7f98fce79c44fdd0a0e447ad6b6
Vojta Stoklasa <dev@vstkl.xyz>
2024-09-27 00:19:51 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+sudo pacman -Syu git base-devel curl
 git clone https://github.com/vstkl/conf ~/.config/ -b arch &&
 ~/.config/packaging.sh
 git clone https://github.com/LazyVim/starter ~/.config/nvim && rm -rf ~/.config/nvim/.git
