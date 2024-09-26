0b1c2223fba97b31e5ffbb0e621a9d930b814631
Vojta Stoklasa <dev@vstkl.xyz>
2024-09-27 00:31:14 +0200

    Update packaging.sh

--- packaging.sh
+++ packaging.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-sudo pacman -Syu git base-devel &&
-git clone https://aur.archlinux.org/yay && cd yay && makepkg -si &&
-yay -Syu pamac-aur --noconfirm
+sudo pacman -Syu git base-devel --noconfirm && git clone https://aur.archlinux.org/yay && cd yay && makepkg -si && yay -Syu pamac-aur --noconfirm
