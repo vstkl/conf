9db850f882cc47f15d5bf8a6f1f99176eb22bf51
vstkl <dev@vstkl.xyz>
2024-09-27 13:05:06 +0200

    typo

--- install_qemu.sh
+++ install_qemu.sh
@@ -1,8 +1,8 @@
-yay -Syu libvirt
+yay -Syu libvirt --noconfirm
 ./install_optdeps.sh libvirt
-yay -R iptables
-yay -Syu iptables-nft openbsd-netcat
-yay -Syu virsh libvirt-sandbox virt-viewer virt-manager cockpit-machines polkit-gnome
+yay -R iptables --noconfirm
+yay -Syu iptables-nft openbsd-netcat --noconfirm
+yay -Syu virsh libvirt-sandbox virt-viewer virt-manager cockpit-machines polkit-gnome --noconfirm
 su - usermod -aG libvirt m
 sudo systemctl start --now libvirtd.service
 sudo systemctl start --now virtlogd.service
